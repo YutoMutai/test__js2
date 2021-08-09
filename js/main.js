44f122bdec48a162f7c34f82233dd2c6272051a5
YutoMutai <youtoo.khs@gmail.com>
2021-08-09 21:06:23 +0900

    記述の修正

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 function leapyear(year) {
-  if ((year % 4 === 0 && year % 100 !== 0 || year % 400 === 0)) {return true}
-return false
+  if ((year % 4 === 0 && year % 100 !== 0 || year % 400 === 0)) { return true; }
+  return false;
 }
-if(LeapYear(2020)){
-  console.log(2020 + '年はうるう年です');
+let checkyear = 2020
+if(leapyear(checkyear)){
+  console.log(checkyear + '年はうるう年です');
 }else{
-  console.log(2020 + '年はうるう年ではありません');
+  console.log(checkyear+ '年はうるう年ではありません');
 }
-if(LeapYear(2021)){
-  console.log(2021 + '年はうるう年です');
+let checkyear2 = 2021
+if(leapyear(checkyear2)){
+  console.log(checkyear2 + '年はうるう年です');
 }else{
-  console.log(2021 + '年はうるう年ではありません');
+  console.log(checkyear2 + '年はうるう年ではありません');
 }
